ad215a982b7989efa28b4626697c76141de17226
Joanna Kuo <jkuo@princeton.edu>
2021-10-04 02:52:55 -0400

    added routes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./views/homePage";
+import AboutPage from "./views/aboutPage";
+import CalendarPage from "./views/calendarPage";
+import PathPage from "./views/pathPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/calendar" component={CalendarPage} />
+        <Route exact path="/path" component={PathPage} />
+      </Switch>
+    </Router>
   );
 }
 
