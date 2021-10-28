4cca7e6a6893dd3a7d4cbb8fc27f0fa30cdbdc9b
Begum Ortaoglu <ortaoglu@princeton.edu>
2021-10-27 21:53:39 -0400

    card align

--- front_end/src/components/path/PathTree/pathTree.js
+++ front_end/src/components/path/PathTree/pathTree.js
@@ -9,16 +9,18 @@ export default function pathTree() {
             <Box sx={{ flex: "0 1 auto", height: "10%" }}>
             <AppBar />
             </Box>
-            <Box sx={{ flex: "1 1 auto", height: "95%" }}>
+            <Box sx={{ flex: "1 1 auto", height: "95%",  }}>
             <Box sx={{ display: "flex", 
                         flexFlow: "column",
                         height: "100%",
-                        justifyContent: 'flex-end'
+                        mt: 3, 
+                        alignItems: 'end'
                         }}>
                     
-                <TreeCard level = '1'/> 
-                <TreeCard level = '2'/>
-                <TreeCard level = '3'/>  
+                <TreeCard level = '1' comp = 'yes' /> 
+                <TreeCard level = '2' comp = 'no' />
+                <TreeCard level = '3' comp = 'yes' />  
+                <TreeCard level = '1' /> 
                 
             </Box>
             </Box>
--- front_end/src/components/path/PathTree/treeCard.js
+++ front_end/src/components/path/PathTree/treeCard.js
@@ -3,11 +3,15 @@ import { Box } from "@mui/material";
 
 export default function BookmarkCard(props) {
     var level = props.level; 
+    var comp = props.comp; 
   return (
     <Box
       sx={{
         m: 0.2,
-        backgroundColor: level == 1? '#D1E7E4' : level == '2' ? 'blue' : 'red',
+        backgroundColor: comp == 'yes' ? '#D1E7E4' : 
+                        comp == 'no' ? '#F6E1E1' : 
+                        comp == 'class'? 'white' : 
+                        '#FBF7D2',
         height: "30px",
         width: level == '1'? "100%" : level == '2' ? "90%" : "80%", 
         borderRadius: 1.5,
