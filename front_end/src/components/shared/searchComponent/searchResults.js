9df80086ec0b145e4f57983c4dbe16a0f47f795a
Begum Ortaoglu <ortaoglu@princeton.edu>
2021-11-05 16:13:04 -0400

    drag and drop progress

--- front_end/src/components/shared/searchComponent/searchResults.js
+++ front_end/src/components/shared/searchComponent/searchResults.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { Box } from "@mui/material";
 import SearchResultCard from "./searchResultCard";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export default function SearchResults() {
+  const results = ["COS126", "MAE345", "ELE206"];
   return (
     <Box>
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
-      <SearchResultCard />
+      {results.map((result) => (
+        <Droppable droppableId="searchBar">
+          {(provided) => (
+            <div
+              className="searchBar"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <SearchResultCard
+                name={result}
+                className={result}
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              />
+                 {provided.placeholder}
+            </div>
+          )}
+       
+        </Droppable>
+      ))}
     </Box>
   );
 }
