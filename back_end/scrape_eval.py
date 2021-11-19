889f37b5aabdd0d3e8d890c99511838c4f6e3064
howard-yen <howardy2000@gmail.com>
2021-11-19 12:16:12 -0500

    Update cookies

--- back_end/scrape_eval.py
+++ back_end/scrape_eval.py
@@ -1,6 +1,10 @@
 import requests
 import browser_cookie3
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 BASE_URL = "https://registrarapps.princeton.edu/course-evaluation?ssub={}&courseinfo={}&terminfo={}"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0"
@@ -29,7 +33,14 @@ if __name__ == "__main__":
     # need to get: DEPT, COURSE CODE, TERM CODE
     url = "https://registrarapps.princeton.edu/course-evaluation?ssub=COS&courseinfo=002051&terminfo=1214"
     headers = {"user-agent": USER_AGENT}
-    cookies = browser_cookie3.load("princeton.edu")
+    cj = browser_cookie3.load()
+    cookies = requests.utils.dict_from_cookiejar(cj)
+    import pdb; pdb.set_trace()
+    if "PHPSESSID" in cookies:
+        cookies = {"PHPSESSID": cookies["PHPSESSID"]}
+    else:
+        cookies = {"PHPSESSID": os.getenv("PHPSESSID")}
+    
 
     session = requests.Session()
     session.cookies.update(cookies)
@@ -39,6 +50,11 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(response.text, 'html.parser')
     stats = soup.find_all("table", {"class": "sr-only sr-only-focusable"})
+
+    def get_course_evaluation(tag):
+        return tag.caption is not None and "Course Evaluation Results" in tag.caption.text
+    temp = soup.find_all(get_course_evaluation)
+    import pdb; pdb.set_trace()
 
     print(stats[0])
     quality_names = stats[0].find_all("th")
