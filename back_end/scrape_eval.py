80e815f8f8b6a03b319f50085522a3f1e9809bbd
howard-yen <howardy2000@gmail.com>
2021-11-19 16:56:26 -0500

    Refactor scrape_eval and added getting instructors.

--- back_end/scrape_eval.py
+++ back_end/scrape_eval.py
@@ -22,57 +22,64 @@ def scrape_eval(session, dept, course_code, term_code):
             All the course evaluations as a dictionary.
     """
     url = BASE_URL.format(dept, course_code, term_code)
+    output = {}
+
     response = session.get(url)
+    if response.status_code != 200:
+        raise AssertionError(f"Expected status code 200 in response, got {response.status_code}")
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    if not "Office of the Registrar" in soup.header.text.strip():
+        raise AssertionError(f"Expected \"Office of the Registrar\" in the response header. Check the response to see if the evaluation page was retrieved correctly. Header found: {soup.header.text.strip()}")
 
-    pass
+    course = soup.find_all("div", {"class": "course"})
+    if len(course) != 1:
+        raise AssertionError(f"Expected to only find one div with classname=course, you probably want to check the code and make sure it's up to date.")
+    course = course[0]
+
+    # get all the ratings
+    stats = course.find_all("table")
+    if len(stats) != 2:
+        raise AssertionError(f"Expected to find two tables in course stats, you probably want to check the code and make sure it's up to date.")
+    
+    quality_names = stats[0].find_all("th")
+    qualities = stats[0].find_all("td")
+    for q_name, q_rating in zip(quality_names, qualities):
+        output[q_name.text] = float(q_rating.text)
+
+    # get all the instructors (should have this info already but might be useful)
+    instructors = course.find_all("li", {"class": "instructor"})
+    output["instructors"] = []
+    for i in instructors:
+        output["instructors"].append(i.text.strip())
+    if len(output["instructors"]) == 0:
+        raise AssertionError(f"Expected to find more than one instructors for the course, but found none.")
+
+    comments_section = soup.find_all("div", {"class": "comments-section mt-4"})
+
+    return output
 
 if __name__ == "__main__":
     # need to get: DEPT, COURSE CODE, TERM CODE
     url = "https://registrarapps.princeton.edu/course-evaluation?ssub=COS&courseinfo=002051&terminfo=1214"
     headers = {"user-agent": USER_AGENT}
+
+    # Set up the cookies for getting past CAS
     cj = browser_cookie3.load()
     cookies = requests.utils.dict_from_cookiejar(cj)
-    import pdb; pdb.set_trace()
     if "PHPSESSID" in cookies:
         cookies = {"PHPSESSID": cookies["PHPSESSID"]}
     else:
         cookies = {"PHPSESSID": os.getenv("PHPSESSID")}
     
-
     session = requests.Session()
     session.cookies.update(cookies)
     session.headers.update(headers)
 
-    response = session.get(url)
+    dept = "COS"
+    course_code="002051"
+    term_code="1214"
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    stats = soup.find_all("table", {"class": "sr-only sr-only-focusable"})
+    scrape_eval(session=session, dept=dept, course_code=course_code, term_code=term_code)
 
-    def get_course_evaluation(tag):
-        return tag.caption is not None and "Course Evaluation Results" in tag.caption.text
-    temp = soup.find_all(get_course_evaluation)
-    import pdb; pdb.set_trace()
-
-    print(stats[0])
-    quality_names = stats[0].find_all("th")
-    qualities = stats[0].find_all("td")
-    
-    print([q.text for q in quality_names])
-    print([q.text for q in qualities])
-
-    output = {}
-    output["current_year"] = {}
-    for k, v in zip(quality_names, qualities):
-        output["current_year"][k.text] = float(v.text)
-
-    # Trying to pull the class comments 
-    comments_section = soup.find_all("div", {"class": "comments-section mt-4"})
-
-    print(comments_section)
-    print(comments_section[0])
-
-    
-    
