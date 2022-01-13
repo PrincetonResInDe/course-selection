ba9f5fcf5b9fe9e257c7cac368445a3af0286e23
Joanna Kuo <jkuo@princeton.edu>
2022-01-12 21:22:05 -0500

    implemented dragging within course list

--- front_end/src/components/calendar/courseListComponent/courseList.js
+++ front_end/src/components/calendar/courseListComponent/courseList.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography, Divider } from "@mui/material";
 import CourseListCard from "./courseListCard";
 import { useCalendarStore } from "../../../zustand/calendar";
 
 export default function CourseList() {
   const [searchWidth] = useCalendarStore((state) => [state.searchWidth]);
+
+  const [data, setData] = useState([
+    { id: 1, number: "MAT 202", name: "Linear Algebra" },
+    {
+      id: 2,
+      number: "COS 126",
+      name: "Computer Science: An Interdisciplinary Approach",
+    },
+    { id: 3, number: "NEU 201", name: "Introduction to Neuroscience" },
+    { id: 4, number: "MAT 202", name: "Linear Algebra" },
+    {
+      id: 5,
+      number: "COS 126",
+      name: "Computer Science: An Interdisciplinary Approach",
+    },
+    { id: 6, number: "NEU 201", name: "Introdcution to Neuroscience" },
+    { id: 7, number: "MAT 202", name: "Linear Algebra" },
+    {
+      id: 8,
+      number: "COS 126",
+      name: "Computer Science: An Interdisciplinary Approach",
+    },
+    { id: 9, number: "NEU 201", name: "Introdcution to Neuroscience" },
+  ]);
+
+  const moveCard = (dragIndex, hoverIndex) => {
+    const dragItem = data[dragIndex];
+
+    if (dragItem) {
+      setData((prevState) => {
+        const copy = [...prevState];
+        const prevItem = copy.splice(hoverIndex, 1, dragItem);
+        copy.splice(dragIndex, 1, prevItem[0]);
+
+        return copy;
+      });
+    }
+  };
 
   return (
     <Box
@@ -25,18 +63,18 @@ export default function CourseList() {
       </Typography>
       <Divider />
 
-      <Box sx={{ flexGrow: 1, overflow: "auto", mb: 1 }}>
+      <Box sx={{ flexGrow: 1, overflow: "auto", mb: 1, p: 1 }}>
         <Box sx={{ height: 0 }}>
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
-          <CourseListCard />
+          {data.map((d, i) => {
+            return (
+              <CourseListCard
+                key={d.id}
+                data={d}
+                index={i}
+                moveCard={moveCard}
+              />
+            );
+          })}
         </Box>
       </Box>
     </Box>
--- front_end/src/components/calendar/courseListComponent/courseListCard.js
+++ front_end/src/components/calendar/courseListComponent/courseListCard.js
@@ -1,14 +1,87 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Card, Typography, Box, Checkbox } from "@mui/material";
+import { useDrag, useDrop } from "react-dnd";
 
-export default function CourseListCard() {
+export default function CourseListCard(props) {
+  const ref = useRef(null);
+
+  const [, drop] = useDrop({
+    accept: "COURSE_CARD",
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = props.index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      props.moveCard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "COURSE_CARD",
+    item: {
+      index: props.index,
+      id: props.data.name,
+    },
+
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
   return (
-    <Card
+    <Box
+      ref={ref}
       sx={{
-        m: 1,
-        padding: 1,
-        boxShadow: "none",
+        opacity: isDragging ? 0.4 : 1,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "background.paper",
+        p: 1,
+        cursor: isDragging ? "grabbing" : "grab",
+        mb: 1,
+        borderRadius: 1,
         "&:hover": {
+          borderRadius: 1,
           boxShadow:
             "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
         },
@@ -16,63 +89,61 @@ export default function CourseListCard() {
     >
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
+          flex: "0 1 auto",
+          overflow: "hidden",
+          display: "inline-grid",
+          width: "100%",
         }}
       >
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Box
+        <Box
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            width: "100%",
+            color: "color.blue",
+            display: "flex",
+            flexDireciton: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            variant="body1"
             sx={{
+              fontWeight: "bold",
+              color: "color.blue",
+              display: "inline",
               overflow: "hidden",
               textOverflow: "ellipsis",
               whiteSpace: "nowrap",
-              width: "100%",
-              color: "color.blue",
-              display: "flex",
-              flexDireciton: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
             }}
           >
-            <Typography
-              variant="body1"
-              sx={{
-                fontWeight: "bold",
-                color: "color.blue",
-                display: "inline",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-              }}
-            >
-              MAT 202
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              overflow: "hidden",
-              textOverflow: "ellipsis",
-              whiteSpace: "nowrap",
-              color: "color.blue",
-            }}
-          >
-            <Typography
-              variant="body1"
-              sx={{
-                color: "color.blue",
-                display: "inline",
-              }}
-            >
-              Linear Algebra
-            </Typography>
-          </Box>
+            {props.data.number}
+          </Typography>
         </Box>
-        <Box>
-          <Checkbox size="small" sx={{ p: 0.1 }} />
+        <Box
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            color: "color.blue",
+          }}
+        >
+          <Typography
+            variant="body1"
+            sx={{
+              color: "color.blue",
+              display: "inline",
+            }}
+          >
+            {props.data.name}
+          </Typography>
         </Box>
       </Box>
-    </Card>
+      <Box>
+        <Checkbox size="small" sx={{ p: 0.1 }} />
+      </Box>
+    </Box>
   );
 }
