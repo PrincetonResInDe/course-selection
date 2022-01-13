f22220e9af69d4bf9b814e2c5eee94cafb7df844
Joanna Kuo <jkuo@princeton.edu>
2022-01-12 21:28:23 -0500

    implemented drag and drog in bookmark list

--- front_end/src/components/calendar/bookmarkListComponent/bookmarkCard.js
+++ front_end/src/components/calendar/bookmarkListComponent/bookmarkCard.js
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Card, Typography, Box } from "@mui/material";
+import { useDrag, useDrop } from "react-dnd";
 
-export default function BookmarkCard() {
+export default function BookmarkCard(props) {
+  const ref = useRef(null);
+
+  const [, drop] = useDrop({
+    accept: "COURSE_CARD",
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = props.index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      props.moveCard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "COURSE_CARD",
+    item: {
+      index: props.index,
+      id: props.data.name,
+    },
+
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
   return (
     <Card
+      ref={ref}
       sx={{
+        opacity: isDragging ? 0.4 : 1,
         m: 1,
         padding: 1,
         boxShadow: "none",
@@ -45,7 +112,7 @@ export default function BookmarkCard() {
               whiteSpace: "nowrap",
             }}
           >
-            MAT 202
+            {props.data.number}
           </Typography>
         </Box>
         <Box
@@ -63,7 +130,7 @@ export default function BookmarkCard() {
               display: "inline",
             }}
           >
-            Linear Algebra
+            {props.data.name}
           </Typography>
         </Box>
       </Box>
