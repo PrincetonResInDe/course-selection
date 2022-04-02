f6896fbe3299894547b2cd86ed3695ade2340513
rebecca0323 <rebeccaazhu@gmail.com>
2022-04-02 16:20:47 -0400

    calendar

--- front_end/src/views/calendarPage.js
+++ front_end/src/views/calendarPage.js
@@ -19,46 +19,30 @@ export default function CalendarPage() {
         backgroundColor: "background.default",
       }}
     >
-      <Search />
-      <Box sx={{ flexGrow: 1, mt: 2, mb: 2, mr: 2 }}>
-        <Calendar />
+      <Search setShow={setShow} show={show} />
+      <Box sx={{ width: "70vw", mt: 2, mb: 2, mr: 2 }}>
+        {!show ? <Calendar /> : <ReviewPage />}
       </Box>
       <Box
         sx={{
+          flexGrow: 1,
+
+          height: "100%",
           display: "flex",
-          flexDirection: "row",
-          height: "100vh",
-          backgroundColor: "background.default",
-          position: "relative",
-          top: 0,
-          left: 0,
+          flexDirection: "column",
         }}
       >
-        <Search setShow={setShow} show={show} />
-        <Box sx={{ width: "70vw", mt: 2, mb: 2, mr: 2 }}>
-          {!show ? <Calendar /> : <ReviewPage />}
+        <Box sx={{ height: "50%", mb: 1 }}>
+          <CourseList />
         </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Box sx={{ height: "50%", mb: 1 }}>
-            <CourseList />
-          </Box>
-          <Box sx={{ flex: "1 1 auto", height: "94%" }}>
-            <Box sx={{ display: "flex", flexFlow: "column", height: "100%" }}>
-              <Box sx={{ height: "49%" }}>
-                <CourseList />
-              </Box>
-              <Box sx={{ mb: 2, height: "1%" }}></Box>
-              <Box sx={{ height: "48%" }}>
-                <BookmarkList />
-              </Box>
+        <Box sx={{ flex: "1 1 auto", height: "94%" }}>
+          <Box sx={{ display: "flex", flexFlow: "column", height: "100%" }}>
+            <Box sx={{ height: "49%" }}>
+              <CourseList />
+            </Box>
+            <Box sx={{ mb: 2, height: "1%" }}></Box>
+            <Box sx={{ height: "48%" }}>
+              <BookmarkList />
             </Box>
           </Box>
         </Box>
